7c26b74e7af262e457fb132ff2cb3927a7c875de
staffanberglund <staffan.l.berglund@gmail.com>
2023-05-05 22:24:13 +0200

    New and improved

--- intonation/intonation.js
+++ intonation/intonation.js
@@ -4,6 +4,8 @@ const { midiToNoteName,  } = Tonal.Midi;
 
 const { Scale, Chord, Note, Range, Key, Interval, Midi } = Tonal;
 
+const { round, floor, random } = Math;
+
 /*------------------------------*/
 /* 	Spela upp ljud		*/
 /*------------------------------*/
@@ -13,15 +15,18 @@ const audioContext = new AudioContext();
 
 let dropdownList = document.getElementById('select_box');
 let sound = 'acoustic_grand_piano';//'voice_oohs'
-dropdownList.onchange = (ev) => {
-	sound = dropdownList.value;
-};
 
-Promise.all([
-	Soundfont.instrument(audioContext, sound).then(function (x) {
-		voice = x;
-	}),
-]);
+//Promise.all([
+//	Soundfont.instrument(audioContext, sound).then(function (x) {
+//		voice = x;
+//	}),
+//]);
+
+async function loadInstruments() {
+    voice = await Soundfont.instrument(audioContext, sound);
+}
+
+loadInstruments();
 
 let adjust;
 let adjust2;
@@ -31,39 +36,76 @@ let adjust2;
 //
 //document.getElementById('slider').value = 1;
 
-const slump = Math.floor(Math.random() + 1);
+const slump = floor(random() + 1);
 const negPos = slump == 0 ? -1 : 1 ;
-adjust = negPos * Math.round(Math.random() * 50 )/100;
-adjust2 = negPos * Math.round(Math.random() * 25 )/100;
+adjust = negPos * round(random() * 50 )/100;
+adjust2 = negPos * round(random() * 25 )/100;
 let ack;
 ack = [60, 64 - adjust, 67 - adjust2];
 
-
-function sliderChange(note, val) {
-	//document.getElementById('output').innerHTML = val; // get
-	//adjust = val;
-	//ack = [60, 64 - adjust];
-	const adj = note == 64 ? adjust : adjust2;
-	const index = note == 64 ? 1 : 2;
-	ack[index] = note - adj + Number(val);
-	console.log(ack[1]);
+function sliderChange(index, val) {
+	const adj = index == 1 ? adjust : adjust2;
+	const ton = index == 1 ? 64 : 67 ;
+	ack[index] = ton - adj + Number(val);
+	console.log(ack[index]);
+	visaAckord();
 };
 
-function intPlus() {
-	ack[1] = ack[1] + 0.01;
-	console.log(ack[1]);
+function intPlus(index) {
+	const par = index == 1 ? 'slider' : 'sliderK';
+	let t = Number(document.getElementById(par).value);
+	let s = t + 0.01;
+	document.getElementById(par).value = s;
+	ack[index] = round((ack[index] + 0.01 ) * 100 ) / 100;
+	visaAckord();
 };
 
-function intMinus() {
-	ack[1] = ack[1] -  0.01;
-	console.log(ack[1]);
+function intMinus(index) {
+	const par = index == 1 ? 'slider' : 'sliderK';
+	let t = Number(document.getElementById(par).value);
+	let s = t - 0.01;
+	document.getElementById(par).value = s;
+	ack[index] = round((ack[index] - 0.01 ) * 100 ) / 100;
+	visaAckord();
 };
 
-document.getElementById('slider').value = 0; // set
-document.getElementById('sliderK').value = 0; // set
+document.getElementById('slider' ).value = 0;
+document.getElementById('sliderK').value = 0;
+
+let ren;
+let lik;
 
 function visaAckord() {
-	document.getElementById('visaAckord').innerHTML = [(ack[1] - ack[0]) * 100 - 386, (ack[2] - ack[0]) * 100 - 702].map(x => Math.round(x)); // set
+	ren = [(ack[1] - ack[0]) * 100 - 386, (ack[2] - ack[0]) * 100 - 702].map(x => round(x));
+	lik = [(ack[1] - ack[0]) * 100 - 400, (ack[2] - ack[0]) * 100 - 700].map(x => round(x));
+
+	document.getElementById('renTers' ).innerHTML = ren[0];
+	document.getElementById('renKvint').innerHTML = ren[1];
+	document.getElementById('likTers' ).innerHTML = lik[0];
+	document.getElementById('likKvint').innerHTML = lik[1];
+};
+
+visaAckord();
+
+function togglePar() {
+  let x = document.getElementById("visaAckord");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+function toggleTemp() {
+  let x = document.getElementById("ren");
+  let y = document.getElementById("lik");
+  if (x.style.display === "none") {
+    x.style.display = "table-row";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "table-row";
+  }
 };
 
 async function chord() {
@@ -76,6 +118,13 @@ async function chord() {
         await Promise.all(playSound);
 };
 
+function väljInstrument() {  
+let instrumentLista = document.getElementById("instrumentLista");  
+	sound = instrumentLista.options[instrumentLista.selectedIndex].text  ;
+	loadInstruments();
+}  
+
+/*
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
@@ -94,4 +143,4 @@ function filterFunction() {
       a[i].style.display = "none";
     }
   }
-}
+}*/
